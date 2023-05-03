b85a0621c96f606bdd6de2d57c87aefe0951bd32
SenchaPencha <SenchaPencha1@gmail.com>
2023-05-03 20:58:22 +0300

    Final

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import requests
+from selenium.webdriver.common.by import By
 from selenium import webdriver
-import unittest
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(options=chrome_options)
 
 
-class BrokenImagesTest(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome(executable_path="path/to/chromedriver")
+driver.get("https://the-internet.herokuapp.com/broken_images")
+image_list = driver.find_elements(By.TAG_NAME, "img")
+print('Total number of images on the page are ' + str(len(image_list)))
 
-    def test_broken_images(self):
-        self.driver.get("https://example.com")
-        images = self.driver.find_elements_by_tag_name("img")
+broken_images = []
+for img in image_list:
+    try:
+        response = requests.get(img.get_attribute('src'), stream=True)
+        if response.status_code != 200:
+            broken_images.append(img.get_attribute('outerHTML'))
+            print(img.get_attribute('outerHTML') + " is broken.")
+    except requests.exceptions.MissingSchema:
+        print("Encountered MissingSchema Exception")
+    except requests.exceptions.InvalidSchema:
+        print("Encountered InvalidSchema Exception")
+    except:
+        print("Encountered Some other Exception")
 
-        for image in images:
-            src = image.get_attribute("src")
-            try:
-                response = requests.get(src)
-                if response.status_code != 200:
-                    print("Broken image:", src)
-            except Exception as e:
-                print("Error checking image:", src)
-                print(e)
+driver.quit()
 
-    def tearDown(self):
-        self.driver.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+print('The page has ' + str(len(broken_images)) + ' broken images')
